7048401a4688b9bd08e8449fa9e5a6d6ae828bf9
Thanakorn Thanprasit <thanakorn_thanprasit@ascendinter.com>
2016-06-23 15:25:30 +0700

    Display data from Object Class

--- src/app/angular2-workshop.component.ts
+++ src/app/angular2-workshop.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {Hero} from './entity/hero.entity';
 
 @Component({
-  moduleId: module.id,
-  selector: 'angular2-workshop-app',
-  templateUrl: 'angular2-workshop.component.html',
-  styleUrls: ['angular2-workshop.component.css']
+	moduleId: module.id,
+	selector: 'angular2-workshop-app',
+	templateUrl: 'angular2-workshop.component.html',
+	styleUrls: ['angular2-workshop.component.css']
 })
 export class Angular2WorkshopAppComponent {
-  title = 'Tour of Heroes';
-  hero = 'Windstorm';
+	title = 'Tour of Heroes';
+	hero : Hero = {
+		id: 1,
+		name: 'Windstorm'
+	};
 }
