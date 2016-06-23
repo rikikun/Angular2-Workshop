f93e11eec4378078fd0e00067017ab6e6a8402ea
Thanakorn Thanprasit <thanakorn_thanprasit@ascendinter.com>
2016-06-23 15:59:54 +0700

    add hero.service via use Service injection to component and retrieve data

--- src/app/angular2-workshop.component.ts
+++ src/app/angular2-workshop.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
-import {Hero,HEROES} from './entity/hero.entity';
+import { Component, OnInit } from '@angular/core';
+import {Hero} from './entity/hero.entity';
 import {HeroDetailComponent} from './components/hero-detail';
+import {HeroService} from './service/hero.service';
 
 @Component({
 	moduleId: module.id,
 	selector: 'angular2-workshop-app',
 	templateUrl: 'angular2-workshop.component.html',
 	styleUrls: ['angular2-workshop.component.css'],
-	directives:[HeroDetailComponent]
+	directives:[HeroDetailComponent],
+	providers:[HeroService]
 })
-export class Angular2WorkshopAppComponent {
+export class Angular2WorkshopAppComponent implements OnInit {
 	title = 'Tour of Heroes';
-	heroes = HEROES;
+	heroes:Hero[];
+
+	constructor(private heroService:HeroService) {}
+
+	ngOnInit() {
+		this.heroes=this.heroService.getHeroes();
+	}
 
 	selectedHero:Hero;
 
